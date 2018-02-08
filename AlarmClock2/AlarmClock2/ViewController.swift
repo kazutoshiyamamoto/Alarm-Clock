ae1844ea3affdaf7e42da825d7592dc9c3551465
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-02-08 16:42:08 +0900

    viewcontroller関連付け

--- AlarmClock2/AlarmClock2/ViewController.swift
+++ AlarmClock2/AlarmClock2/ViewController.swift
@@ -13,8 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
+        
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -29,4 +28,4 @@ class ViewController: UIViewController {
     }
     
 }
-
+}
