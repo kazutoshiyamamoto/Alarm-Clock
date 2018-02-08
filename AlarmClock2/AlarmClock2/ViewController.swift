1f75e134e81b2d06aa654218742e815231eb96e3
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-02-08 17:02:44 +0900

    関連付けに関する修正

--- AlarmClock2/AlarmClock2/ViewController.swift
+++ AlarmClock2/AlarmClock2/ViewController.swift
@@ -13,12 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBOutlet weak var alarmLabel: UILabel!
     
     @IBAction func settingButtonAction(_ sender: Any) {
@@ -26,6 +26,4 @@ class ViewController: UIViewController {
     
     @IBAction func switchButtonAction(_ sender: Any) {
     }
-    
-}
 }
