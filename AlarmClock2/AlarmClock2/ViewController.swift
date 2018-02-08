1bf318e53f34f683887cb2771346fcf4c20796c4
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-02-08 15:35:24 +0900

    アラーム画面/設定画面の関連付け

--- AlarmClock2/AlarmClock2/ViewController.swift
+++ AlarmClock2/AlarmClock2/ViewController.swift
@@ -20,6 +20,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var alarmLabel: UILabel!
+    
+    @IBAction func settingButtonAction(_ sender: Any) {
+    }
+    
+    @IBAction func switchButtonAction(_ sender: Any) {
+    }
+    
 }
 
