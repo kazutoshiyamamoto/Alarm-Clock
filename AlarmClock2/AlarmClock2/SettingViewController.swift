99282f5471acb14332c091533f0e11eb84e84f81
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-02-13 23:56:01 +0900

    設定画面の作成

--- AlarmClock2/AlarmClock2/SettingViewController.swift
+++ AlarmClock2/AlarmClock2/SettingViewController.swift
@@ -21,6 +21,14 @@ class SettingViewController: UIViewController, UIPickerViewDelegate, UIPickerVie
         // Do any additional setup after loading the view.
     }
 
+    // DatePickerのデリゲートとデータソースの通知先を設定
+    datePicker.delegate = self
+    datePicker.dataSource = self
+    
+    // UserDefaultsの取得
+    let settings = UserDefaults.standard
+    let timerValue = settings.integer(forKey: settingKey)
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
