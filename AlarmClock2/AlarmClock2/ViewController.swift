3c0e750e4a5f56da27c2d350fd9f57c64dd6f9ae
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-02-14 21:38:44 +0900

    アラームの変数の作成/経過時間を記録する変数の作成/秒数を保持するときに利用するキーの作成/起動時の初期値を設定

--- AlarmClock2/AlarmClock2/ViewController.swift
+++ AlarmClock2/AlarmClock2/ViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // アラームの変数を作成
+    var timer : Timer?
+    // カウント（経過時間）の変数を作成
+    var count = 0
+    // 設定値を扱うキーを設定
+    let settingKey = "timer_value"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // UserDefaultsのインスタンスを生成
+        let settings = UserDefaults.standard
+        // UserDefaultsに初期値を登録
+        settings.register(defaults: [settingKey: NSDate()])
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
